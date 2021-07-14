5da2cb6b28ec63e19b1f42e69d559c90c2fb52b7
teviw000 <31836504+teviw000@users.noreply.github.com>
2021-07-14 16:50:14 -0500

    added redux toolkit to change the way we use redux in general. made it shorter & concise. easier to work with redux now

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,38 +1,39 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 
-import classes from './Counter.module.css';
- 
+import { counterActions } from '../store/index';
+import classes from "./Counter.module.css";
+
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector(state => state.counter);
+  const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: 'increment' });
-  }
+    dispatch(counterActions.increment());
+  };
 
   const increaseHandler = () => {
-    dispatch({ type: 'increase', amount: 5});
-  }
-
+    dispatch(counterActions.increase(10)); // { type: SOME_UNIQUE_IDENTIFIER, payload: 10}
+  };
 
   const decrementHandler = () => {
-    dispatch({ type: 'decrement' });
-  }
+    dispatch(counterActions.decrement());
+  };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
-        <button onClick={incrementHandler} >Increment</button>
-        <button onClick={increaseHandler} >Increase by 5</button>
-        <button onClick={decrementHandler} >Decrement</button>
-        
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 10</button>
+        <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
-      
     </main>
   );
 };
